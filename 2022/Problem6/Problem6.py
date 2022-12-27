d277b7d668234ce394fe5e9cbf73169978bec93f
Lipetka <liptak2000@gmail.com>
2022-12-27 19:11:31 +0100

    Problem 6 solution for both parts

---
+++ 2022/Problem6/Problem6.py
@@ -0,0 +1,24 @@
+import string
+################ PART 1 ################
+
+def main():
+    input_file = 'Problem6\input.txt'           # path to input file
+    data = open(input_file,'r').read()          # open input file
+    dataLength = 14                             # change to 14 for part 2
+
+    for i in range(len(data)):
+        
+        checkedSubString = data[i:i+dataLength] # extract substring to be checked
+        duplicateFlag = 0                       # duplicate character flag
+
+        for char in checkedSubString:
+            if checkedSubString.count(char) > 1:
+                # if there are more duplicate characters raise flag
+                duplicateFlag = 1
+        
+        # if there are no duplications return index i and break loop
+        if duplicateFlag == 0:
+            print(i+dataLength)
+            break
+
+main()
